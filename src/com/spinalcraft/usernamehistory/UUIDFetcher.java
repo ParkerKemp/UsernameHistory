2a36dd20b0d023a6c12b3cf504a0c6eda4829e91
ParkerKemp <charles.kemp702@topper.wku.edu>
2015-04-03 19:49:12 -0500

    UUIDFetcher license

--- src/com/spinalcraft/usernamehistory/UUIDFetcher.java
+++ src/com/spinalcraft/usernamehistory/UUIDFetcher.java
@@ -1,11 +1,26 @@
-package com.spinalcraft.usernamehistory;
+/*
+ * Copyright (c) 2015 Nate Mortensen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-//This is a modified version of a class created by evilmidget38:
-//
-//https://gist.github.com/evilmidget38/26d70114b834f71fb3b4
-//
-//I don't know if I need to do give any official kind of credit, but
-//I'd feel dirty if I didn't at least say this much.
+package com.spinalcraft.usernamehistory;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
